64366b7f4372678135388f4009f2cf76e5ab9a1a
abodabulawi4-eng <abodabulawi4@gmail.com>
2025-09-21 18:45:33 -0700

    Resolve merge conflict in index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
-// src/index.js
-
-=======
->>>>>>> bfc3347bbbbe4449ab8b6d941be3a7c990d79b27
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
-<<<<<<< HEAD
 import customTheme from './theme'; // استيراد الثيم المخصص
-=======
->>>>>>> bfc3347bbbbe4449ab8b6d941be3a7c990d79b27
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <ChakraProvider theme={customTheme}>
-=======
-    <ChakraProvider>
->>>>>>> bfc3347bbbbe4449ab8b6d941be3a7c990d79b27
       <App />
     </ChakraProvider>
   </React.StrictMode>
